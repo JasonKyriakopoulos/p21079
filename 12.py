984e5c6e7512a8d3d0703b0b96db54f42a92d9d5
JasonKyriakopoulos <100002577+JasonKyriakopoulos@users.noreply.github.com>
2022-02-19 01:33:33 +0200

    Update 12.py

--- 12.py
+++ 12.py
@@ -22,12 +22,11 @@ def toBinary(a):
 def RandomToString(Round2,Luck2):
 
  for i in range(1,100):
-
-     req = Request('https://drand.cloudflare.com/public/' + str(Round2-1), headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20130401 Firefox/31.0'})
-     data = urlopen(req).read()
-     keys = json.loads(data.decode('utf-8'))
-     Round2=keys['round']
-     Luck2=Luck2+keys['randomness']
+  req = Request('https://drand.cloudflare.com/public/' + str(Round2-1), headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20130401 Firefox/31.0'})
+  data = urlopen(req).read()
+  keys = json.loads(data.decode('utf-8'))
+  Round2=keys['round']
+  Luck2=Luck2+keys['randomness']
  toBinary(Luck2)
 
 
